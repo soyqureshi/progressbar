2a42c8790259babb7c3f91a0fd8f0548a20253e9
soyqureshi <77929398+soyqureshi@users.noreply.github.com>
2021-02-19 23:38:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function fastScroll (){
+    document.querySelector ("#bbar").style.animation = "fasty 1s linear infinite alternate";
+}
+
+function slowScroll (){
+    document.querySelector ("#bbar").style.animation = "fasty 10s linear infinite alternate";
+}
+
+function resetScroll (){
+    document.querySelector ("#bbar").style.animation = "0";
+}
+
+
+
+
